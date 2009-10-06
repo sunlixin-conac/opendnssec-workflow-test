b658a7757bc436e171e1ae5f6a9c344d695aa295
sion <sion@b8cecbbd-7941-0410-8e65-efea935c1ce0>
2009-10-06 19:46:24 +0000

    new test configuration
    git-svn-id: svn+ssh://svn.opendnssec.org/svn/dnssec@2110 b8cecbbd-7941-0410-8e65-efea935c1ce0

--- home/sion/sion_run_lite.sh
+++ home/sion/sion_run_lite.sh
@@ -4,48 +4,32 @@ MYDIR=/home/sion/temp/opendnssec
 
 export LD_LIBRARY_PATH=$MYDIR/install/lib:/home/sion/work/subversion/ldns/lib
 export SOFTHSM_CONF=$MYDIR/install/etc/opendnssec/softhsm.conf
+#export ENFORCER_TIMESHIFT=now
 
 cd $MYDIR
 
 echo "Test1: mysql DB in conf.xml"
-cp $MYDIR/trunk/testing/enforcer/examples/lite-sion-conf.xml $MYDIR/install/etc/opendnssec/conf.xml
-cp $MYDIR/trunk/testing/enforcer/examples/kasp.xml $MYDIR/install/etc/opendnssec/kasp.xml
-cp $MYDIR/trunk/testing/enforcer/examples/sion-softhsm.conf $MYDIR/install/etc/opendnssec/softhsm.conf
-cp $MYDIR/trunk/testing/enforcer/examples/sion-zonelist.xml $MYDIR/install/etc/opendnssec/zonelist.xml
+cp $MYDIR/home/sion/testing/lite-sion-conf.xml $MYDIR/install/etc/opendnssec/conf.xml
+cp $MYDIR/home/sion/testing/kasp.xml $MYDIR/install/etc/opendnssec/kasp.xml
+cp $MYDIR/home/sion/testing/sion-softhsm.conf $MYDIR/install/etc/opendnssec/softhsm.conf
+cp $MYDIR/home/sion/testing/sion-zonelist.xml $MYDIR/install/etc/opendnssec/zonelist.xml
 
-#$MYDIR/install/bin/softhsm --init-token --slot 0 --label "alice"
+$MYDIR/install/bin/softhsm --init-token --slot 0 --label "alice"
+$MYDIR/install/bin/softhsm --init-token --slot 1 --label "alice2"
 
-#Import the example xml from /tmp/jad/opendnssec/etc/opendnssec/ in to the DB
-#$MYDIR/install/bin/kaspimport -f $MYDIR/install/var/opendnssec/enforcer.db -i
+#Import the xml config from $MYDIR in to the DB
+$MYDIR/install/bin/ksmutil setup
 
-#Start the key generator
-$MYDIR/install/sbin/keygend -u sion -d &
-
-sleep 10
-killall -9 keygend
-echo "Look in the system logs and see what happened."
-
-echo "Running communicator\n"
-
-echo "THIS SHOULD BE SORTED SOMEWHERE ELSE!!!\nCREATING CONF DIRECTORY\n"
-mkdir $MYDIR/install/var/opendnssec/config
-
-$MYDIR/install/sbin/communicated -u sion -P comm.pid -d &
+#Start the enforcerd
+$MYDIR/install/sbin/enforcerd -d &
 
 sleep 10
-killall -9 communicated
+
+$MYDIR/install/bin/ksmutil backup done
+
+killall -HUP enforcerd
+
+sleep 10
+killall -INT enforcerd
 echo "Look in the system logs and see what happened."
 
-#
-#echo "Test2: mysql DB in conf.xml"
-#cp $MYDIR/trunk/testing/enforcer/examples/mysql-bad-conf.xml $MYDIR/install/etc/opendnssec/conf.xml
-#
-##Import the example xml from /tmp/jad/opendnssec/etc/opendnssec/ in to the DB
-#$MYDIR/install/bin/kaspimport -h localhost -u ksm_test -p ksm_test -s ksm -i
-#
-##Start the key generator
-#$MYDIR/install/bin/keygend -u sion -d &
-#
-#sleep 10
-#killall -9 keygend
-#echo "Look in the system logs and see what happened."
