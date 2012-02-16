99d896bf9270803e497807159b915cb061d1e736
jakob <jakob@b8cecbbd-7941-0410-8e65-efea935c1ce0>
2012-02-16 21:25:06 +0000

    release OpenDNSSEC 1.3.6
    git-svn-id: svn+ssh://svn.opendnssec.org/svn/dnssec@6172 b8cecbbd-7941-0410-8e65-efea935c1ce0

---
+++ releases/OpenDNSSEC-1.3.6/testing/test-opendnssec.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+source `dirname "$0"`/lib.sh && init || exit 1
+source `dirname "$0"`/functions-opendnssec.sh || exit 1
+
+require opendnssec
+
+check_if_tested opendnssec && exit 0
+start_test opendnssec
+
+PRE_TEST=ods_pre_test
+POST_TEST=ods_post_test
+test_ok=0
+(
+	run_tests test-cases.d
+) &&
+test_ok=1
+
+stop_test
+
+if [ "$test_ok" -eq 1 ]; then
+	set_test_ok opendnssec || exit 1
+	exit 0
+fi
+
+exit 1
