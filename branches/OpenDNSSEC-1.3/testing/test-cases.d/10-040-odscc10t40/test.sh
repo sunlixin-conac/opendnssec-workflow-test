45ccf75ee92b320bc2790bd67a2853079395ce88
jerry <jerry@b8cecbbd-7941-0410-8e65-efea935c1ce0>
2012-02-21 13:53:45 +0000

    More support functions for opendnssec tests. Fixed trunk tests. Added tests to 1.3 branch.
    git-svn-id: svn+ssh://svn.opendnssec.org/svn/dnssec@6180 b8cecbbd-7941-0410-8e65-efea935c1ce0

--- branches/OpenDNSSEC-1.3/testing/test-cases.d/10-040-odscc10t40/test.sh
+++ branches/OpenDNSSEC-1.3/testing/test-cases.d/10-040-odscc10t40/test.sh
@@ -3,10 +3,6 @@
 # Configure no module location and expect failure
 
 ods_reset_env &&
-ods_setup_conf "conf.xml" "broken-conf.xml" &&
-
-echo "y" | ods-ksmutil setup &&
-return 1
 
 log_this ods-control-start ods-control start &&
 syslog_waitfor 2 'ods-enforcerd: .*PKCS#11 module load failed: '"$INSTALL_ROOT/var/libsofthsm.so" &&
